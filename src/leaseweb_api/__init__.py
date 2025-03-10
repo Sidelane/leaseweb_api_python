74be061e18b47a40796fc2de0eefb4f3150a4908
Sidelane <36916365+Sidelane@users.noreply.github.com>
2025-03-10 15:33:17 +0100

    Implement dedicated_services/launch_rescue_mode

--- src/leaseweb_api/__init__.py
+++ src/leaseweb_api/__init__.py
@@ -1,4 +1,4 @@
-from .dedicated_services import DedicatedServices, DedicatedServers
+from .dedicated_servers import DedicatedServices, DedicatedServers
 from .auth_provider import LeasewebAuthenticationProvider
 from .types.enums import DetectionProfile
 from .types.credentials import Credential, CredentialWithoutPassword, CredentialType
